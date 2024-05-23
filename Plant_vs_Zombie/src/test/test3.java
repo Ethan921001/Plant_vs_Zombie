d4e894a3aa61274972e81aeb302f98105dde2d9a
zzzhk <zzzhk@Hankk2>
2024-05-23 16:59:16 +0800

    新增MusicPlayer

--- Plant_vs_Zombie/src/test/test3.java
+++ Plant_vs_Zombie/src/test/test3.java
@@ -1,10 +1,13 @@
 package test;
 
 import model.system.GameStage;
-
+import model.system.MusicPlayer;
 public class test3 {
 	public static void main(String[] args) {
 		GameStage stage = new GameStage();
+		MusicPlayer player = new MusicPlayer();
+		player.music_play();
 		stage.play_game();
+		
 	}
 }
