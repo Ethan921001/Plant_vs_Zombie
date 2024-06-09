830a751f23ae0190a28115f9b89092b5744b108b
LiAnNO2 <ru03m4h@gmail.com>
2024-06-09 16:59:05 +0800

    GameOver

--- Plant_vs_Zombie/src/model/system/GameStage.java
+++ Plant_vs_Zombie/src/model/system/GameStage.java
@@ -26,7 +26,7 @@ public class GameStage {
 	private PlantFactory plant_factory;
 	private MapView map_view;
 	private MenuView menu_view;
-	private GameoverWindow gameoverWindow;
+	private GameOverWindow gameoverWindow;
 	
 	private PauseWindow pauseWindow;
 	private Judger judger;
@@ -53,7 +53,7 @@ public class GameStage {
 		map_view=new MapView(zombies,plants,bullets,cards,economySystem,judger,shovel,fertilizer,timer);	
 		menu_view=new MenuView();
 		pauseWindow=new PauseWindow(map_view.getMusicPlayer());
-		gameoverWindow=new GameoverWindow();
+		gameoverWindow=new GameOverWindow();
 		
 		//map_view.add(new PauseButton());
 	
